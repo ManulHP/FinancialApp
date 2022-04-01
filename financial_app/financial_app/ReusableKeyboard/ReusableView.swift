b3e0a11b8940fd70eadc3ef45dd229acb4dfdf8e
Manul Pallege <60056336+ManulHP@users.noreply.github.com>
2022-04-01 16:07:02 +0800

    REUSABLE KEYBOARD ADDED

---
+++ financial_app/financial_app/ReusableKeyboard/ReusableView.swift
@@ -0,0 +1,20 @@
+//
+//  ReusableView.swift
+//  financial_app
+//
+//  Created by user214203 on 4/1/22.
+//
+
+import UIKit
+
+class ReusableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
