70be9db7237de6e1b210e31a14668bbecf97e9fd
Manul Pallege <60056336+ManulHP@users.noreply.github.com>
2022-04-03 14:00:28 +0800

    help description added

---
+++ financial_app/financial_app/About/About.swift
@@ -0,0 +1,22 @@
+//
+//  About.swift
+//  financial_app
+//
+//  Created by user214203 on 4/3/22.
+//
+
+import Foundation
+
+
+final class AboutConstant {
+    
+    static let SavingTitile = "Investment"
+    static let SvingDescription = "A financial investment/fixed-savings is an asset that you put money into with the hope that it will grow or appreciate into a larger sum of money."
+    
+    static let CopoundSavingTitiel = "Savings"
+    static let SompoundSavingDescription = "Calculates the future value of your savings account based on the present value, interest rate and number of terms. Will consider 12 compounds for a year"
+    
+    static let LoanTitle = "Mortgage"
+    static let LoanDescription = "A mortgage is a loan in which property or real estate is used as collateral. The borrower enters into an agreement with the lender wherein the borrower receives cash upfront then makes payments over a set time span until he pays back the lender in full."
+    
+}
