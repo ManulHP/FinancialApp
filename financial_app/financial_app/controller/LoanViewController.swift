692a95254d50ab74b9c3cf85b961d5834846c9a9
Manul Pallege <60056336+ManulHP@users.noreply.github.com>
2022-04-04 18:59:55 +0800

    comment added

--- financial_app/financial_app/controller/LoanViewController.swift
+++ financial_app/financial_app/controller/LoanViewController.swift
@@ -19,23 +19,15 @@ class LoanViewController: UIViewController {
         
         keyboard.delegate = self
 
+        // disable the default iOS keyboard
         textfields.forEach { textfield in
             textfield.inputView = UIView()
             textfield.inputAccessoryView = UIView()
         }
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
