0b3c4102219a422569511621f3c0294e1bc4bf38
Manul Pallege <60056336+ManulHP@users.noreply.github.com>
2022-04-08 13:16:17 +0800

    help page added

--- financial_app/financial_app/controller/AboutViewController.swift
+++ financial_app/financial_app/controller/AboutViewController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    
+    @IBOutlet var loanView: UIView!
+    @IBOutlet var compoundView: UIView!
+    @IBOutlet var saveView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        viewSettings()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public func viewSettings() {
+        saveView.layer.cornerRadius = 20
+        saveView.layer.borderColor = UIColor.black.cgColor
+        saveView.layer.borderWidth = 3.5
+        saveView.layer.masksToBounds = true
+        
+        compoundView.layer.cornerRadius = 20
+        compoundView.layer.borderColor = UIColor.black.cgColor
+        compoundView.layer.borderWidth = 3.5
+        compoundView.layer.masksToBounds = true
+        
+        loanView.layer.cornerRadius = 20
+        loanView.layer.borderColor = UIColor.black.cgColor
+        loanView.layer.borderWidth = 3.5
+        loanView.layer.masksToBounds = true
     }
-    */
 
 }
